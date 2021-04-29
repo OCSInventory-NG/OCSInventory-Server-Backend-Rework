8d127f807f32deaade72de5c63d15557ee57043e
Gilles Dubois <gilles.dubois@factorfx.com>
2021-04-29 10:24:18 +0200

    Add default serializer definition

---
+++ user/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    This serialize class provide the API representation
+
+    Args:
+        serializers ([HyperlinkedModelSerializer])
+    """
+    class Meta:
+        """
+        Define the linked model and the fields registered in the API
+        """
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
