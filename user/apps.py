020a085e75480f08f56992c0d7d3266d96629183
Gilles Dubois <gilles.dubois@factorfx.com>
2021-04-29 10:23:00 +0200

    PyDoc on app

--- user/apps.py
+++ user/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
-
 class UserConfig(AppConfig):
+    """
+    Base definition of the django app
+
+    Args:
+        AppConfig ([AppConfig])
+    """
     name = 'user'
