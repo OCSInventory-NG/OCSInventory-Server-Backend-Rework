a6c7bcfd503024927d196704764a18f7adf3fd98
Gilles Dubois <gilles.dubois@factorfx.com>
2021-04-29 10:23:11 +0200

    Add suerViewSet and PyDoc

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from user.serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([ModelViewSet])
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
