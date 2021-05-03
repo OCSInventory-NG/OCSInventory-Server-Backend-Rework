ddade63e555ae7b713052a327a96ced3203a7c11
Léa <ljdroguet@gmail.com>
2021-05-03 17:28:34 +0200

    create method for user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,16 +1,40 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers 
+from rest_framework import serializers
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     """
     This serialize class provide the API representation
 
     Args:
-        serializers ([HyperlinkedModelSerializer])
+        serializers ([ModelSerializer])
     """
     class Meta:
         """
         Define the linked model and the fields registered in the API
         """
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name',  'password', 'is_staff']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+    def create(self, validated_data):
+        """
+        Override existing create method to ensure password is encrypted
+
+        Args:
+            validated_data : POST request
+
+        Returns:
+            [user]
+        """
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            is_staff=validated_data['is_staff'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
