0dd3ba65cfe42a2e534ed705349bd8bdb7df1246
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-06 15:29:06 +0200

    Add IsAuthenticated param in userview to restrict access

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from user.serializers import UserSerializer
 
 
@@ -10,5 +11,8 @@ class UserViewSet(viewsets.ModelViewSet):
     Args:
         viewsets ([ModelViewSet])
     """
+
+    permission_classes = [IsAuthenticated] # Need to be authenticated to consult
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
