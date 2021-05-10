0b60ef053490a09ce38a145b22730bf4470de246
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-10 15:13:48 +0200

    Remove useless import

--- group/views.py
+++ group/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
