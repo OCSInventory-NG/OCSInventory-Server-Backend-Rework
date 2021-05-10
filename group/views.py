21d9d7a0e1b917df9f4119c4feb71023c34e530f
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-10 14:31:35 +0200

    First iteration of the group API

---
+++ group/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import Group
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from group.serializers import GroupSerializer
+from django.contrib.auth.models import Permission
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([ModelViewSet])
+    """
+
+    # Need to be authenticated to consult
+    permission_classes = [IsAuthenticated]
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
