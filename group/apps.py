8e94dad3b490066342ab9c9a9e138e29bb87d9b4
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-10 14:34:10 +0200

    Add docstring

--- group/apps.py
+++ group/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class GroupConfig(AppConfig):
+    """
+    Base definition of the django app
+
+    Args:
+        AppConfig ([AppConfig])
+    """
     name = 'group'
