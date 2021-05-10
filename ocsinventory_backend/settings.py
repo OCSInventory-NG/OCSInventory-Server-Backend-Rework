0e8eab0b99191c598671dec3df074cdced214a45
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-10 14:31:00 +0200

    Add django model for model level permissions

--- ocsinventory_backend/settings.py
+++ ocsinventory_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'user.apps.UserConfig',
+    'group.apps.GroupConfig',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissions',
     ]
 }
 
