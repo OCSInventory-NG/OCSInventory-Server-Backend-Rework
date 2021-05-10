631d36d551d33ae4d0e3aa39d8417f5f691eb119
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-10 15:14:53 +0200

    Add permission listing for frontend

---
+++ permission/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class PermissionConfig(AppConfig):
+    """
+    Base definition of the django app
+
+    Args:
+        AppConfig ([AppConfig])
+    """
+
+    name = 'permission'
