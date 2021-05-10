23f322e7463aaec4ba82f5794451a016688fbb3d
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-10 15:45:18 +0200

    Remove useless imports

--- group/views.py
+++ group/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import Group
 from rest_framework import viewsets
-from rest_framework import status
 from permission.permissions import DefaultModelPermissions
-from rest_framework.response import Response
 from group.serializers import GroupSerializer
 
 
