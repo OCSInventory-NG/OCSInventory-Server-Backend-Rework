20797520c32ea58c660df4fcfce7996b28041ca1
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-11 16:59:14 +0200

    Fix docstring errors

--- config/apps.py
+++ config/apps.py
@@ -2,4 +2,11 @@ from django.apps import AppConfig
 
 
 class ConfigConfig(AppConfig):
+    """
+    Base definition of the django app
+
+    Args:
+        AppConfig ([AppConfig])
+    """
+
     name = 'config'
--- config/serializers.py
+++ config/serializers.py
@@ -11,5 +11,7 @@ class ConfigSerializer(serializers.ModelSerializer):
     """
 
     class Meta:
+        """Define the linked model and the fields registered in the API"""
+        
         model = Config
         fields = ['name', 'value']
