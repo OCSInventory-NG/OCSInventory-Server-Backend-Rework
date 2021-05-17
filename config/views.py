e2577f47d86b18d3225a7bc1e1ec1490bf38f358
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-17 16:17:13 +0200

    Cleanup user / group / config views

--- config/views.py
+++ config/views.py
@@ -1,13 +1,10 @@
-from rest_framework import viewsets
 from permission.permissions import DefaultModelPermissions
 from config.serializers import ConfigSerializer
 from config.models import Config
-
-# alias ocs_viewsets avoids conflict w/ rest_framework's imported viewsets
-from ocsinventory_backend.ocs_framework import viewsets as ocs_viewsets
+from ocsinventory_backend.ocs_framework import viewsets 
 
 
-class ConfigViewSet(ocs_viewsets.OCSViewSet):
+class ConfigViewSet(viewsets.OCSViewSet):
     """
     This class will define the view behavior
 
