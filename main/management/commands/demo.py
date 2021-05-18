4540f3c08523d6034cf0203b8bb49542e1c552c0
Léa <ljdroguet@gmail.com>
2021-05-18 14:41:43 +0200

    add comments

--- main/management/commands/demo.py
+++ main/management/commands/demo.py
@@ -2,11 +2,17 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'test this please'
+    """Name of the file equals command, e.g. 'demo'
+
+    Args:
+        BaseCommand ([type]): base class for management commands
+    """
+    help = 'any arg passed to this cmd will be printed back'
 
     def add_arguments(self, parser):
-        parser.add_argument('msg', type=str, help='insert help msg')
+        parser.add_argument('msg', type=str, help='string')
 
     def handle(self, *args, **options):
         msg = options['msg']
+        # let's just print back the arg
         self.stdout.write(str(msg))
