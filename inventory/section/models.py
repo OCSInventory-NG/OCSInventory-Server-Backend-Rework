7219458c00f9cfe6322530942b84a0c9a80ab7bb
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-27 10:54:16 +0200

    Add base section model

--- inventory/section/models.py
+++ inventory/section/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from inventory.template import models
+
 
 # Create your models here.
+class Section:
+    """
+    Template model class definition
+
+    The model will contain the following info
+    - Name
+    - Operating system
+    - Sections template link
+    - Revision (Read Only)
+    """
+    RETRIVAL_CHOICES = (
+        ("FILE", "Read file"),
+        ("BASH", "Bash command"),
+        ("PW", "Powershell command"),
+        ("CMD", "Cmd command"),
+    )
+
+    name = models.CharField(max_length=50)
+    retrival_method = models.CharField(
+        max_length=4,
+        choices=RETRIVAL_CHOICES,
+        default="FILE"
+    )
+    target = models.CharField(
+        max_length=255
+    )
+    template = models.ForeignKey(models.Template, on_delete=models.CASCADE)
