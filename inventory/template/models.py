97807bb124e27ad71ff2bf468f69fcd95f3b2445
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-27 10:44:04 +0200

    Add template base model without foreign keys

--- inventory/template/models.py
+++ inventory/template/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Template:
+    """
+    Template model class definition
+
+    The model will contain the following info
+    - Name
+    - Operating system
+    - Sections template link
+    - Revision (Read Only)
+    """
+    OS_CHOICES = (
+        ("WIN", "Windows"),
+        ("LIN", "Linux"),
+        ("MAC", "Mac"),
+    )
+
+    name = models.CharField(max_length=50)
+    os = models.CharField(
+        max_length=3,
+        choices=OS_CHOICES,
+        default="WIN"
+    )
+    revision = models.IntegerField()
