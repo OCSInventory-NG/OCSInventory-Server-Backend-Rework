27de2a55a91e7bbdb9f70b11da83d67ba21ecb0e
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-27 16:35:29 +0200

    Add basic viewset for templates

--- inventory/template/views.py
+++ inventory/template/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from permission.permissions import DefaultModelPermissions
+from inventory.template.models import Template
+from inventory.template.serializers import TemplateSerializer
+from ocsinventory_backend.ocs_framework import viewsets
 
-# Create your views here.
+
+class TemplateViewSet(viewsets.OCSViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSVIewSet])
+    """
+
+    # Need to have permissions to consult
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = Template.objects.all()
+    serializer_class = TemplateSerializer
+    model = Template
