40e571468f024dede9720ff7832226e9370061ee
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-27 17:51:42 +0200

    Add template link and heritage

--- inventory/section/views.py
+++ inventory/section/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from permission.permissions import DefaultModelPermissions
+from inventory.section.models import Section
+from inventory.section.serializers import SectionSerializer
+from ocsinventory_backend.ocs_framework import viewsets
 
-# Create your views here.
+
+class SectionViewSet(viewsets.OCSViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSVIewSet])
+    """
+
+    # Need to have permissions to consult
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+    model = Section
