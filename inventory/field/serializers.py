ffc95714ea0cde0cb3769a5ffb408bbb729d980d
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-28 17:09:49 +0200

    Add default serializer

---
+++ inventory/field/serializers.py
@@ -0,0 +1,22 @@
+from inventory.field.models import Field
+from rest_framework import serializers
+
+
+class FieldSerializer(serializers.ModelSerializer):
+    """
+    This serialize class provide the API representation
+
+    Args:
+        serializers ([ModelSerializer])
+    """
+
+    class Meta:
+        """Define the linked model and the fields registered in the API"""
+
+        model = Field
+        fields = [
+            'id',
+            'name',
+            'retrival_value',
+            'section'
+        ]
