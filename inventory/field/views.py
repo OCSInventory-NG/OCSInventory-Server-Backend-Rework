2778eb6201bc09305339511de75e53208211ff0b
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-28 17:09:19 +0200

    Add default field viewset

--- inventory/field/views.py
+++ inventory/field/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from permission.permissions import DefaultModelPermissions
+from inventory.field.models import Field
+from inventory.field.serializers import FieldSerializer
+from ocsinventory_backend.ocs_framework import viewsets
 
-# Create your views here.
+
+class FieldViewSet(viewsets.OCSViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSVIewSet])
+    """
+
+    # Need to have permissions to consult
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = Field.objects.all()
+    serializer_class = FieldSerializer
+    model = Field
