679872e0ea39448d5af348abaa72e71374cbaa54
Gilles Dubois <gilles.dubois@factorfx.com>
2021-05-31 16:49:43 +0200

    Add base viewset

--- asset/base/views.py
+++ asset/base/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from permission.permissions import DefaultModelPermissions
+from asset.base.models import Base
+from asset.base.serializers import BaseSerializer
+from ocsinventory_backend.ocs_framework import viewsets
 
-# Create your views here.
+
+class BaseViewSet(viewsets.OCSViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSVIewSet])
+    """
+
+    # Need to have permissions to consult
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = Base.objects.all()
+    serializer_class = BaseSerializer
+    model = Base
