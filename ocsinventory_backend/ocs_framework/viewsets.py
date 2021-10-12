ab633e77a4e4f3b55637f37bd19114b6d7eb9c3f
Gilles Dubois <gilles.dubois@factorfx.com>
2021-10-12 09:55:30 +0200

    Set RaiseException to False

--- ocsinventory_backend/ocs_framework/viewsets.py
+++ ocsinventory_backend/ocs_framework/viewsets.py
@@ -38,7 +38,7 @@ class OCSViewSet(viewsets.ModelViewSet):
         try:
             serializer = self.get_serializer(
                 data=request.data, many=isinstance(request.data, list))
-            serializer.is_valid(raise_exception=True)
+            serializer.is_valid(raise_exception=False)
             self.perform_create(serializer)
         except (APIException, FieldError, ObjectDoesNotExist):
             # serializer.errors may return more details
