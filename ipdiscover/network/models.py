520d26b7b945fc04a5e424be8e971ebe39469e27
Gilles Dubois <gillesdubois18@gmail.com>
2021-10-15 15:23:01 +0200

    Add possibility to create network without groups

--- ipdiscover/network/models.py
+++ ipdiscover/network/models.py
@@ -19,4 +19,4 @@ class Network(models.Model):
     netid = models.GenericIPAddressField()
     mask = models.GenericIPAddressField()
     group = models.ForeignKey(
-        Netgroup, related_name="netgroups", on_delete=models.CASCADE)
+        Netgroup, related_name="netgroups", on_delete=models.CASCADE, null=True)
