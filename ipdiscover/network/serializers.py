5f6c56406a24e03dfae7e7f2fb9f397ff3c58275
Gilles Dubois <gillesdubois18@gmail.com>
2021-10-15 15:23:18 +0200

    Add nested devices insertion

--- ipdiscover/network/serializers.py
+++ ipdiscover/network/serializers.py
@@ -1,4 +1,6 @@
 from ipdiscover.network.models import Network
+from ipdiscover.netgroup.serializers import NetgroupSerializer
+from ipdiscover.netdevice.serializers import NetdeviceSerializer
 from rest_framework import serializers
 
 
@@ -10,6 +12,9 @@ class NetworkSerializer(serializers.ModelSerializer):
         serializers ([ModelSerializer])
     """
 
+    group = NetgroupSerializer(required=False)
+    netdevices = NetdeviceSerializer(many=True, required=False)
+
     class Meta:
         """Define the linked model and the fields registered in the API"""
 
@@ -20,6 +25,22 @@ class NetworkSerializer(serializers.ModelSerializer):
             'description',
             'netid',
             'mask',
+            'netdevices',
             'group'
         ]
+
+    def create(self, validated_data):
+        """Override create to allow nested creation of fields"""
+        if 'netdevices' in validated_data.keys():
+            # If netdevices are present
+            netdevices = validated_data.pop('netdevices')
+            parent = super().create(validated_data)
+
+            for netdevice in netdevices:
+                netdevice['network'] = parent
+            self.fields['netdevices'].create(netdevices)
+        else:
+            parent = super().create(validated_data)
+
+        return parent
 
