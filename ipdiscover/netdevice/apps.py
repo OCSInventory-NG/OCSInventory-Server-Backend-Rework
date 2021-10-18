5867bd406c34aa351e6bd971e782bf914f8ac704
Gilles Dubois <gilles.dubois@factorfx.com>
2021-10-18 15:21:39 +0200

    Add docstrings

--- ipdiscover/netdevice/apps.py
+++ ipdiscover/netdevice/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class NetdeviceConfig(AppConfig):
+    """
+    Base definition of the django app
+
+    Args:
+        AppConfig ([AppConfig])
+    """
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ipdiscover.netdevice'
