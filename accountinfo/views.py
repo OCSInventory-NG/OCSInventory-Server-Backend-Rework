d1f29563574fe7823f36dddf4c71759bc19fab8b
Gilles Dubois <gilles.dubois@factorfx.com>
2021-12-07 14:25:42 +0100

    Add default viewSet

--- accountinfo/views.py
+++ accountinfo/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from permission.permissions import DefaultModelPermissions
+from accountinfo.models import AccountinfoConfig, AccountinfoData, AccountinfoValue
+from accountinfo.serializers import AccountinfoConfigSerializer, AccountinfoDataSerializer, AccountinfoValueSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class AccountinfoConfigViewSet(viewsets.ModelViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSVIewSet])
+    """
+
+    # Need to have permissions to consult
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = AccountinfoConfig.objects.all()
+    serializer_class = AccountinfoConfigSerializer
+    model = AccountinfoConfig
+
+class AccountinfoValueViewSet(viewsets.ModelViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSVIewSet])
+    """
+
+    # Need to have permissions to consult
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = AccountinfoValue.objects.all()
+    serializer_class = AccountinfoValueSerializer
+    model = AccountinfoValue
+
+class AccountinfoDataViewSet(viewsets.ModelViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSVIewSet])
+    """
+
+    # Need to have permissions to consult
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = AccountinfoData.objects.all()
+    serializer_class = AccountinfoDataSerializer
+    model = AccountinfoData
