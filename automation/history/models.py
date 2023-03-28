ddca437ad20f3e9b3f69326b9bbedcc439ffc104
RudyLaurent <rudy@factorfx.com>
2023-03-28 15:05:11 +0200

    refactor(automation/history): change task to scheduler

--- automation/history/models.py
+++ automation/history/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from automation.tasks.models import Tasks
+from automation.scheduler.models import Scheduler
 
 # Create your models here.
 class History(models.Model):
-    task = models.ForeignKey(Tasks, on_delete=models.CASCADE, null=True)
+    task = models.ForeignKey(Scheduler, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField()
     comment = models.CharField(max_length=255)
