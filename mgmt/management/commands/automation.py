39d25ee9b518a8df21e71acf67e2c2f1b55083ec
RudyLaurent <rudy@factorfx.com>
2023-03-28 17:16:59 +0200

    refactor(automation): add comment

--- mgmt/management/commands/automation.py
+++ mgmt/management/commands/automation.py
@@ -6,17 +6,26 @@ from datetime import datetime, timedelta
 import pytz
 
 class Command(BaseCommand):
+    """Name of the file equals command, e.g. 'demo'
+
+    Args:
+        BaseCommand ([type]): base class for management commands
+    """
+
     help = 'Test command'
     library = "automation.tasks."
     utc=pytz.UTC
 
     def handle(self, *args, **options):
+        """Execute all Tasks"""
         tasks = Scheduler.objects.all()
         for task in tasks:
+            #Set up task name
             name = task.name
             upperName = task.name.capitalize()
             completeName = self.library + name + "." + upperName
 
+            # Configure delta time by recurence
             if(task.recurence == 'hourly'):
                 delta = timedelta(hours=1)
             elif(task.recurence == 'daily'):
@@ -28,21 +37,29 @@ class Command(BaseCommand):
 
             now = datetime.now().replace(tzinfo=self.utc)
 
+            # Check if task need to be start
             if(task.last_exec is None or task.last_exec.replace(tzinfo=self.utc) + delta < now):
+                # Save history
                 self.updateHistory(task, "Start")
+
+                # Import module task
                 taskClass = module_loading.import_string(completeName)
+                # Execute task command
                 taskClass.execute()
 
+                # Update execution date
                 task.last_exec = datetime.now(tz=self.utc)
+                # Save new date
                 task.save()
 
+                # Save finish history
                 self.updateHistory(task, "End")
-                print("Done")
     
-    def updateHistory(task, comment):
-        h = History(
-            task=task, 
-            comment=comment
-        )
-        h.save()
+        def updateHistory(task, comment):
+            """Update history with task and comment"""
+            h = History(
+                task=task, 
+                comment=comment
+            )
+            h.save()
             
