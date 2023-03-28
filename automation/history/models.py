19fecd28642e884625f6bf8286fb980a50e113f5
RudyLaurent <rudy@factorfx.com>
2023-03-28 17:05:09 +0200

    feat(automation): set up auto date in history model

--- automation/history/models.py
+++ automation/history/models.py
@@ -4,5 +4,5 @@ from automation.scheduler.models import Scheduler
 # Create your models here.
 class History(models.Model):
     task = models.ForeignKey(Scheduler, on_delete=models.CASCADE, null=True)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now=True)
     comment = models.CharField(max_length=255)
