41951799efb8794cbb85b4d2f707398b27d81e9a
RudyLaurent <rudy@factorfx.com>
2023-03-28 09:56:36 +0200

    feat(automation/scheduler): update views with scheduler values

--- automation/scheduler/views.py
+++ automation/scheduler/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from automation.scheduler.models import Scheduler
+from automation.scheduler.serializers import SchedulerSerializer
+from ocsinventory_backend.ocs_framework import viewsets
+from permission.permissions import DefaultModelPermissions
 
 # Create your views here.
+class SchedulerViewSet(viewsets.OCSViewSet):
+    """
+    This class will define the view behavior
+
+    Args:
+        viewsets ([OCSViewSet])
+    """
+
+    permission_classes = [DefaultModelPermissions]
+
+    queryset = Scheduler.objects.all()
+    serializer_class = SchedulerSerializer
+    model = Scheduler
