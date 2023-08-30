c7a71d85604ab4a3116280a294fcc1f274e9bb52
Gilles Dubois <gilles.dubois@factorfx.com>
2023-08-30 17:09:20 +0200

    refactor(deployment): add os_choices

--- deployment/package/models.py
+++ deployment/package/models.py
@@ -9,10 +9,16 @@ class Package(models.Model):
     - Name
     - Description
     - Date of creation
-    - Target OS
+    - Target OS (limited by OS_CHOICES list)
     """
+    
+    OS_CHOICES = (
+        ("WIN", "Windows"),
+        ("LIN", "Linux"),
+        ("MAC", "Mac"),
+    )
 
     name = models.CharField(max_length=128)
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-    target_os = models.CharField(max_length=128)
+    target_os = models.CharField(max_length=3, choices=OS_CHOICES, default="WIN")
