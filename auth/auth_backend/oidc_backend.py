4c136f38a0a3ee765c1314172e53f0b1e9b3009d
Lea9250 <lea.droguet@factorfx.com>
2023-10-25 14:02:45 +0200

    refactor(auth): use mappings with oidc auth and change reconciliation field for user accordingly

--- auth/auth_backend/oidc_backend.py
+++ auth/auth_backend/oidc_backend.py
@@ -15,8 +15,13 @@ class CustomOIDCBackend(OIDCAuthenticationBackend):
 
         self.configs = AuthConfig.objects.filter(auth_method__name="OIDC",
                                                  enabled=True).order_by("priority")
-        self.mappings = AuthMapping.objects.filter(
+        self.mappings = AuthMapping.objects.filter(auth_config__enabled=True,
                                                    auth_config__auth_method__name="OIDC")
+
+        # dict mapping external fields to internal fields.
+        self.field_mappings = {mapping.internal_field: mapping.external_field
+                               for mapping in self.mappings}
+        
         # TODO : get the first enabled config for now but figure out how to handle multiple configs (or prevent it)
         oidc_config = self.configs[0]
 
@@ -50,17 +55,65 @@ class CustomOIDCBackend(OIDCAuthenticationBackend):
         return None
 
     def filter_users_by_claims(self, claims):
-        """Override this method to filter users by claims"""
-        # TODO : do we let admin user define which claim
-        # shoud be used as reconciliation ?
-        # for now, we use the sub claim
+        """
+        Override mozilla_django_oidc filter_users_by_claims method
+        Return all users matching the reconciliation field
+        """
         print("CustomOIDCBackend.filter_users_by_claims")
-        reconciliation = claims.get("sub")
+
+        reconciliation = self.get_user_reconciliation(claims)
+
         if not reconciliation:
             return self.UserModel.objects.none()
         return self.UserModel.objects.filter(username=reconciliation)
 
     def create_user(self, claims):
-        reconciliation = claims.get("sub")
-        # username = self.get_username(claims)
-        return self.UserModel.objects.create_user(username=reconciliation)
+        """Overriding mozilla_django_oidc create_user method"""
+        print("CustomOIDCBackend.create_user")
+
+        reconciliation = self.get_user_reconciliation(claims)
+
+        # populate the user data
+        user_data = {}
+        for internal, external in self.field_mappings.items():
+            # already handled username
+            if external != "username":
+                user_data[internal] = claims.get(external)
+
+        # emove 'username' from user_data to avoid duplicate argument
+        user_data.pop("username", None)
+
+        # create and return the user using the extracted data
+        return self.UserModel.objects.create_user(username=reconciliation, **user_data)
+
+    def get_user_reconciliation(self, claims):
+        """
+        Custom method to get the user reconciliation field from mappings
+        of the config
+        Multiple cases :
+        - no mapping for username : use sub claim
+        - mapping for username : use the external field mapped to username
+        - no mappings for the config : use sub claim
+        - mapped field not found in claims : use sub claim
+        """
+        print("CustomOIDCBackend.get_user_reconciliation")
+
+        # using username as reconciliation field
+        username_mapping = self.field_mappings.get("username")
+        if username_mapping:
+            # default to using the 'sub' claim if the claim is not found
+            reconciliation = claims.get(username_mapping, claims.get("sub"))
+        else:
+            # if no mapping defined, or no mapping matches 'username',
+            # then we default to using the 'sub' claim
+            self.logger.warning(
+                "No mapping found for internal field 'username'. "
+                "Defaulting to using the 'sub' claim for reconciliation."
+            )
+            reconciliation = claims.get("sub")
+
+        if not reconciliation:
+            raise ValueError("Failed to find a reconciliation value from the OIDC"
+                             " claims or from the configured mappings.")
+
+        return reconciliation
