5276a77b8ac1b927a05627e9ad7eb0e4ffd4f5fb
Lea9250 <lea.droguet@factorfx.com>
2023-11-03 15:12:15 +0100

    feat(serializers): custom data validation in auth method serializer (manage SSO and priority updates)

--- auth/auth_method/serializers.py
+++ auth/auth_method/serializers.py
@@ -14,4 +14,60 @@ class AuthMethodSerializer(serializers.ModelSerializer):
         """Define the linked model and the fields registered in the API"""
 
         model = AuthMethod
-        fields = ["id", "name", "priority", "enabled"]
+        fields = '__all__'
+
+    def validate(self, data):
+        """
+        Validate the data before updating the object
+        Two checks are performed:
+        - If auth_type=SSO, only one method can be enabled
+        - If auth_type=OTHER, PRIORITY must be unique
+        """
+        # check if TYPE=SSO and enforce only one method enabled
+        if data.get('auth_type') == 'SSO':
+            if 'enabled' in data and data['enabled']:
+                existing_sso = AuthMethod.objects.filter(
+                    auth_type='SSO', enabled=True
+                ).exclude(pk=self.instance.pk if self.instance else None)
+                if existing_sso.exists():
+                    raise serializers.ValidationError(
+                        "Another SSO method is already enabled. "
+                        "Please disable it before enabling a new one."
+                    )
+
+        # check PRIORITY uniqueness for non-SSO methods
+        if data.get('auth_type') != 'SSO':
+            priority = data.get('priority')
+            if priority is not None:
+                # Check if there is an existing method with the same priority
+                existing_method = AuthMethod.objects.filter(
+                    priority=priority
+                ).exclude(auth_type='SSO').exclude(pk=self.instance.pk if self.instance
+                                                   else None).first()
+
+                if existing_method:
+                    # if the current priority is being updated to a higher priority
+                    if self.instance and priority < self.instance.priority:
+                        # decrease the priority of all methods with lower priority
+                        AuthMethod.objects.filter(
+                            priority__lt=self.instance.priority,
+                            priority__gte=priority
+                        ).exclude(auth_type='SSO').update(priority=F('priority') + 1)
+
+                    # if the current priority is being updated to a lower priority
+                    elif self.instance and priority > self.instance.priority:
+                        # increase the priority of all methods with higher priority
+                        AuthMethod.objects.filter(
+                            priority__lte=priority,
+                            priority__gt=self.instance.priority
+                        ).exclude(auth_type='SSO').update(priority=F('priority') - 1)
+
+                    # if adding a new method or changing priority
+                    else:
+                        # decrease the priority of all methods with priority equal to
+                        # or higher than the current one
+                        AuthMethod.objects.filter(
+                            priority__gte=priority
+                        ).exclude(auth_type='SSO').update(priority=F('priority') + 1)
+
+        return data
