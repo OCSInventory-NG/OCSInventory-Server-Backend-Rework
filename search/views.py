0673e60cae40edf4c9bd2e9af1e22f593df4764d
Charlene Auger <charlene.auger@factorfx.com>
2024-01-03 16:27:35 +0000

    refactor(search): improve Q object generation

--- search/views.py
+++ search/views.py
@@ -34,29 +34,39 @@ class SearchView(APIView):
 
         self.LOGGER.info("Start search construction query")
 
-        # storing errors
-        # errors = []
-
         data = request.data
 
-        # try:
-        # initialize empty arguments
-        kwargs = {}
+        # Initialisation de la liste des filtres Q
+        filters = []
 
-        for params in data:
-            for param in params:
-                kwargs = {
-                    "{0}__{1}".format(param["field"], param["operator"]): param["value"]
-                }
+        # Itération sur la structure JSON
+        for and_conditions in data:
+            and_filter = Q()
 
-        q_objects = Q()
-        for key, value in kwargs.items():
-            q_objects.add(Q(**{key: value}), Q.AND)
+            # Itération sur les conditions "ET"
+            for condition in and_conditions:
+                field = condition["field"]
+                operator = condition["operator"]
+                value = condition["value"]
 
-        query_set = InventoryBase.objects.filter(q_objects)
+                # Construction de la condition Q
+                condition_q = Q(**{f"{field}__{operator}": value})
+
+                # Si le filtre précédent était lié par "OR", utilisez OR, sinon utilisez AND
+                if condition["link"] == "OR":
+                    and_filter |= condition_q
+                else:
+                    and_filter &= condition_q
+
+            # Ajout du filtre "ET" à la liste des filtres
+            filters.append(and_filter)
+
+        # Construction du filtre final en utilisant ET entre les filtres "OR"
+        q_object = filters[0]
+        for q_filter in filters[1:]:
+            q_object &= q_filter
+
+        query_set = InventoryBase.objects.filter(q_object)
         qs_json = serializers.serialize("json", query_set)
-
-        # except:
-        #    errors.append('Error append in search construction')
 
         return HttpResponse(qs_json, content_type="application/json")
