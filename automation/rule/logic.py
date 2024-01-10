8ec79de00a57e3c7ad506f3066e618c79a7ce60d
Lea9250 <lea.droguet@factorfx.com>
2024-01-10 15:39:11 +0100

    feat(rule): accountinfo data can now be modified by actions

--- automation/rule/logic.py
+++ automation/rule/logic.py
@@ -1,3 +1,4 @@
+from accountinfo.models import AccountinfoData
 from automation.rule.models import Rule
 from json_logic import jsonLogic
 from django.db.models.fields import CharField, IntegerField
@@ -5,6 +6,7 @@ from django.db.models.fields.json import JSONField
 from django.db.models.fields.related import ForeignKey
 from django.core.exceptions import FieldDoesNotExist, ObjectDoesNotExist
 from django.db.models.fields.related import ManyToManyField
+from django.contrib.contenttypes.models import ContentType
 
 import logging
 
@@ -35,12 +37,10 @@ class Logic:
         for rule in rules:
             try:
                 result = jsonLogic(rule.logic, self.instance.__dict__)
-
                 if result:
                     self.execute_actions(rule)
             except Exception as e:
                 self.LOGGER.error(f"Error processing rule: {e}")
-
 
     def execute_actions(self, rule):
         """Execute the actions for the given rule
@@ -57,11 +57,28 @@ class Logic:
         The presence of content_type and object_id indicates that the action
         must be executed on a related instance
         Otherwise, the action is executed on the instance itself
+
+        NB : special treatment for AccountinfoConfig since these are linked to
+        the instance through a GenericForeignKey and the field is a JSONField
         """
         if action.content_type and action.object_id:
             try:
                 model = action.content_type.model_class()
-                related_instance = model.objects.get(id=action.object_id)
+                # special treatment for AccountinfoConfig
+                if model.__name__ == 'AccountinfoConfig':
+                    # get accountinfo data matching the instance id
+                    # if accountinfo data does not exist, create it
+                    already_exists = AccountinfoData.objects.filter(object_id=self.instance.id, content_type=ContentType.objects.get_for_model(self.instance))
+                    if not already_exists.exists():
+                        model_name = self.instance.__class__.__name__.lower()
+                        app_name = self.instance._meta.app_label.lower()
+                        slug = f"{app_name}.{model_name}"
+                        related_instance = AccountinfoData.objects.create(object_id=self.instance.id, content_type=ContentType.objects.get_for_model(self.instance), 
+                                                                          object_slug=slug,
+                                                                          accountdata={})
+                    else:
+                        related_instance = already_exists.first()
+
                 self.update_field(related_instance, action)
             except model.DoesNotExist:
                 self.LOGGER.error(f"Related instance not found: {model.__name__} with ID {action.object_id}")
@@ -80,20 +97,23 @@ class Logic:
         JSONField processing is experimental and attempts to update the
         data in nested JSON structures
         """
-        if ':' in action.field:
-            field, key = action.field.split(':', 1)
-            self.update_json_field(instance, field, key, action.value)
-        else:
-            value = self.convert_value(instance, action.field, action.value)
-            field = instance._meta.get_field(action.field)
-            if isinstance(field, ManyToManyField):
-                # use add() for ManyToManyFields on instance
-                getattr(instance, action.field).add(value)
+        try:
+            if ':' in action.field:
+                field, key = action.field.split(':', 1)
+                self.update_json_field(instance, field, key, action.value)
             else:
-                setattr(instance, action.field, value)
-        # setting processed to True to avoid infinite loop where .save() triggers the post_save signal
-        instance.processed = True
-        instance.save()
+                value = self.convert_value(instance, action.field, action.value)
+                field = instance._meta.get_field(action.field)
+                if isinstance(field, ManyToManyField):
+                    # use add() for ManyToManyFields on instance (group, permissions, etc.)
+                    getattr(instance, action.field).add(value)
+                else:
+                    setattr(instance, action.field, value)
+            # setting processed to True to avoid infinite loop where .save() triggers the post_save signal
+            instance.processed = True
+            instance.save()
+        except Exception as e:
+            self.LOGGER.error(f"Error updating field: {e}")
 
     @staticmethod
     def convert_value(instance, field_name, value):
@@ -138,14 +158,16 @@ class Logic:
     @staticmethod
     def update_json_field(instance, field, key, value):
         """Update the JSON field of the given instance
-
         Warning: this is experimental and attempts to update the data in nested JSON structures
         e.g. updating accountinfo data
         """
-        json_data = getattr(instance, field, {}).copy()
-        keys = key.split(':')
-        data = json_data
-        for k in keys[:-1]:
-            data = data.setdefault(k, {})
-        data[keys[-1]] = value
-        setattr(instance, field, json_data)
+        try:
+            json_data = getattr(instance, field, {}).copy()
+            keys = key.split(':')
+            data = json_data
+            for k in keys[:-1]:
+                data = data.setdefault(k, {})
+            data[keys[-1]] = value
+            setattr(instance, field, json_data)
+        except Exception as e:
+            Logic.LOGGER.error(f"Error updating JSON field: {e}")
