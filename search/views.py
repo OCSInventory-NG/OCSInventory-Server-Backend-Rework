1e001150064625cf2d6317741f190bda7fb722bf
charleneauger <charleneauger@users.noreply.github.com>
2024-02-22 13:17:29 +0000

    [MegaLinter] Apply linters fixes

--- search/views.py
+++ search/views.py
@@ -129,7 +129,11 @@ class SearchView(APIView):
                             **{f"{obj}__template_section__exact": condition["section"]}
                         )
                         condition_q &= Q(
-                            **{f"{obj}__fields__template_field__exact": condition["field"]}
+                            **{
+                                f"{obj}__fields__template_field__exact": condition[
+                                    "field"
+                                ]
+                            }
                         )
                         condition_q &= Q(**{f"{obj}__fields__value__{operator}": value})
                     else:
@@ -160,7 +164,7 @@ class SearchView(APIView):
                 else:
                     q_object &= q_filter
 
-            query_set = InventoryBase.objects.filter(q_object).distinct('pk')
+            query_set = InventoryBase.objects.filter(q_object).distinct("pk")
         else:
             query_set = []
 
