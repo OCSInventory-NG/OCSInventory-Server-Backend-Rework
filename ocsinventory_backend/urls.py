bc239022394ee30e4b82c75fdc140f78201f17bd
Djily SARR <djilysarr275@gmail.com>
2024-04-30 14:39:06 +0000

    Adding legacy route

--- ocsinventory_backend/urls.py
+++ ocsinventory_backend/urls.py
@@ -18,6 +18,7 @@ from accountinfo.routers import AccountinfoRouter
 from asset.agent_config.routers import AgentConfigRouter
 from asset.asset_group.routers import AssetGroupRouter
 from asset.collection.views import CollectionView
+from legacy.collection.views import LegacyView
 from asset.inventory_base.routers import InventoryBaseRouter
 from asset.inventory_field.routers import InventoryFieldRouter
 from asset.inventory_section.routers import InventorySectionRouter
@@ -173,6 +174,7 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api-auth/token", obtain_auth_token, name="api_token_auth"),
     path("asset/collection/", CollectionView.as_view(), name="asset_collection"),
+    path("legacy/collection/", LegacyView.as_view(), name="legacy_collection"),
     path("search/", SearchView.as_view(), name="search"),
     # Authentication
     path("login/", BaseAuthView.as_view(), name="login"),
