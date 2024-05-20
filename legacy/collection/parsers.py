e65d34ad25d54c1a3b834873b5747c8b416b6516
Antoine ROBIN <antoine.robin@factorfx.com>
2024-05-20 15:27:13 +0000

    fix(legacyXMLParser): change the parser to parse stream or non stream

--- legacy/collection/parsers.py
+++ legacy/collection/parsers.py
@@ -12,113 +12,83 @@ class LegacyXMLParser(BaseParser):
     media_type = "application/*"
     LOGGER = logging.getLogger(__name__)
 
+    def get_nested(self, data, keys, default="-1"):
+        """Helper function to get a nested value from a dictionary."""
+        for key in keys:
+            try:
+                data = data[key]
+            except (KeyError, TypeError):
+                return default
+        return data or default
+
+    def get_first_up_network_mac(self, networks):
+        """Helper function to get the MAC address of the first network with status 'Up'."""
+        for network in networks:
+            if network.get("STATUS") == "Up":
+                return network.get("MACADDR", "-1")
+        return "-1"
+
     def parse(self, stream, media_type=None, parser_context=None):
         """
         Parses the incoming bytestream as XML and returns the resulting data.
         """
-        data_stream = stream.read()
-        data = xmltodict.parse(zlib.decompress(data_stream))
+        decompressor = zlib.decompressobj()
+        try:
+            data_stream = decompressor.decompress(stream.read())
+            data_stream += decompressor.flush()
+        except zlib.error as e:
+            print(f"Inflation failed: {e}")
+        data = xmltodict.parse(data_stream.decode("utf-8"))
         # Remove the decompression if you are debugging
-        # data = xmltodict.parse(data_stream)
+        # data = xmltodict.parse(stream.read())
 
         template_data = {}
-        if data["REQUEST"]["QUERY"] == "PROLOG":
-            template_data["query"] = data["REQUEST"]["QUERY"]
-        else:
-            try:
-                template_data["query"] = data["REQUEST"]["QUERY"]
-            except KeyError:
-                template_data["query"] = "-1"
-            try:
-                template_data["name"] = data["REQUEST"]["CONTENT"]["HARDWARE"][
-                    "NAME"
-                ]
-            except KeyError:
-                template_data["name"] = "-1"
-            try:
-                template_data["description"] = data["REQUEST"]["CONTENT"][
-                    "HARDWARE"
-                ]["DESCRIPTION"]
-            except KeyError:
-                template_data["description"] = "-1"
-            try:
-                template_data["serial"] = data["REQUEST"]["CONTENT"]["BIOS"][
-                    "SSN"
-                ]
-            except KeyError:
-                template_data["serial"] = "-1"
-            try:
-                template_data["osname"] = data["REQUEST"]["CONTENT"][
-                    "HARDWARE"
-                ]["OSNAME"]
-            except KeyError:
-                template_data["osname"] = "-1"
-            try:
-                template_data["osversion"] = data["REQUEST"]["CONTENT"][
-                    "HARDWARE"
-                ]["OSVERSION"]
-            except KeyError:
-                template_data["osversion"] = "-1"
-            try:
-                template_data["uuid"] = data["REQUEST"]["CONTENT"]["HARDWARE"][
-                    "UUID"
-                ]
-            except KeyError:
-                try:
-                    for network in data["REQUEST"]["CONTENT"]["NETWORKS"]:
-                        if network["STATUS"] == "Up":
-                            template_data["uuid"] = (
-                                data["REQUEST"]["CONTENT"]["HARDWARE"]["NAME"]
-                                + "_"
-                                + network["MACADDR"]
-                            )
-                            break
-                except KeyError:
-                    template_data["uuid"] = "-1"
+        request_data = data.get("REQUEST", {}).get("CONTENT", {})
 
-            # If the ipaddr contain multiple ip addresses,
-            # split them and add the first one to the dictionary
-            try:
-                ipaddr = data["REQUEST"]["CONTENT"]["HARDWARE"]["IPADDR"].split(
-                    "/"
-                )[0]
-            except KeyError:
-                ipaddr = "-1"
-            template_data["srcip"] = ipaddr
+        # Set the query
+        template_data["query"] = data.get("REQUEST", {}).get("QUERY", "-1")
 
-            # Get mac address if the network is up
-            try:
-                for network in data["REQUEST"]["CONTENT"]["NETWORKS"]:
-                    if network["STATUS"] == "Up":
-                        template_data["srcmac"] = network["MACADDR"]
-                        break
-            except KeyError:
-                template_data["srcmac"] = "-1"
+        if template_data["query"] != "PROLOG":
+            template_data["name"] = self.get_nested(request_data, ["HARDWARE", "NAME"])
+            template_data["description"] = self.get_nested(
+                request_data, ["HARDWARE", "DESCRIPTION"]
+            )
+            template_data["serial"] = self.get_nested(request_data, ["BIOS", "SSN"])
+            template_data["osname"] = self.get_nested(
+                request_data, ["HARDWARE", "OSNAME"]
+            )
+            template_data["osversion"] = self.get_nested(
+                request_data, ["HARDWARE", "OSVERSION"]
+            )
+            template_data["uuid"] = self.get_nested(request_data, ["HARDWARE", "UUID"])
+            if template_data["uuid"] == "-1":
+                macaddr = self.get_first_up_network_mac(
+                    request_data.get("NETWORKS", [])
+                )
+                if macaddr != "-1":
+                    template_data["uuid"] = (
+                        f"{self.get_nested(request_data, ['HARDWARE', 'NAME'])}_{macaddr}"
+                    )
+
+            template_data["srcip"] = self.get_nested(
+                request_data, ["HARDWARE", "IPADDR"]
+            ).split("/")[0]
+
+            template_data["srcmac"] = self.get_first_up_network_mac(
+                request_data.get("NETWORKS", [])
+            )
+
+            template_data["domain"] = self.get_nested(request_data, ["HARDWARE", "DNS"])
 
             try:
-                template_data["domain"] = data["REQUEST"]["CONTENT"][
-                    "HARDWARE"
-                ]["DNS"]
-            except KeyError:
-                template_data["domain"] = "-1"
-
-            try:
-                # retrieve template id where template's name is legacy
-                template_data["template"] = Template.objects.get(
-                    name="Legacy"
-                ).id
-            except KeyError:
-                template_data["template"] = "-1"
+                template_data["template"] = Template.objects.get(name="Legacy").id
             except ObjectDoesNotExist:
                 self.LOGGER.error("Template legacy not found")
-                return Response(
-                    {"error": "Template legacy  not found"}, status=404
-                )
+                return Response({"error": "Template legacy not found"}, status=404)
             except Exception as e:
                 self.LOGGER.error(f"Error retrieving template legacy: {e}")
                 return Response(
-                    {"error": f"Error retrieving template legacy: {e}"},
-                    status=500,
+                    {"error": f"Error retrieving template legacy: {e}"}, status=500
                 )
 
             # Transforming the section to a list if it is a dictionary
@@ -129,7 +99,4 @@ class LegacyXMLParser(BaseParser):
                 else:
                     template_inventory[key] = value
             template_data["template_inventory"] = template_inventory
-
-        self.LOGGER.info("test")
-
         return template_data
