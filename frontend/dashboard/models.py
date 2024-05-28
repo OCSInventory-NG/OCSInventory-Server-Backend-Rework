2ec0c5f6f307ff71859b7bc55fed879e9bd40c49
Antoine ROBIN <antoine.robin@factorfx.com>
2024-05-28 13:05:21 +0000

    fix(dashboard model): use restrctvisibility model for user foreignkey

--- frontend/dashboard/models.py
+++ frontend/dashboard/models.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
+from ocsinventory_backend.ocs_framework.models import RestrictVisibility
 from django.db import models
 
 # Create your models here.
 
 
-class Dashboard(models.Model):
+class Dashboard(RestrictVisibility):
     """
     This is the dashboard model.
     """
 
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False)
     name = models.CharField(max_length=100, blank=False, null=False)
     layout = models.JSONField(blank=False, null=False)
