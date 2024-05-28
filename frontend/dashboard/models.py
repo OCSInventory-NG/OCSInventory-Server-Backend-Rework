b164060f4be15c2288bdc9c2ff940f0a29c33bea
Antoine ROBIN <antoine.robin@factorfx.com>
2024-05-28 12:52:13 +0000

    feat(dashboard model): add user key

--- frontend/dashboard/models.py
+++ frontend/dashboard/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -8,6 +9,6 @@ class Dashboard(models.Model):
     This is the dashboard model.
     """
 
-    # user = models.ForeignKey()
-    name = models.CharField(max_length=100)
-    layout = models.JSONField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    layout = models.JSONField(blank=False, null=False)
