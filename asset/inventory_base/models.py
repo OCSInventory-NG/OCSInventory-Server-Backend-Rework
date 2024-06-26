a1e07f6ecf0141989b3207fb6b1e7b259ef35d7f
Lea9250 <lea.droguet@factorfx.com>
2024-06-26 14:17:30 +0200

    refactor(InventoryBase): allow blank values for osversion, srcmac, and domain

--- asset/inventory_base/models.py
+++ asset/inventory_base/models.py
@@ -22,16 +22,16 @@ class InventoryBase(models.Model):
     - Template
     - Domain
     """
-
+ 
     name = models.CharField(max_length=50, null=False)
     description = models.CharField(max_length=255)
     serial = models.CharField(max_length=255)
     osname = models.CharField(max_length=255, null=False)
-    osversion = models.CharField(max_length=255, null=False)
+    osversion = models.CharField(max_length=255, blank=True)
     uuid = models.CharField(max_length=255, null=False, unique=True)
     srcip = models.CharField(max_length=255)
-    srcmac = models.CharField(max_length=255)
-    domain = models.CharField(max_length=255)
+    srcmac = models.CharField(max_length=255, blank=True)
+    domain = models.CharField(max_length=255, blank=True)
     template = models.ForeignKey(
         Template, on_delete=models.CASCADE, blank=True, null=True
     )
