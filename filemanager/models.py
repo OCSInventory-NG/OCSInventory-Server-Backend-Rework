92a392255315c1a4f0e20bf688ede177cd70b50c
Lea9250 <lea.droguet@factorfx.com>
2024-10-25 16:02:53 +0200

    refactor(FileManager): add upload_to method for dynamic upload_path

--- filemanager/models.py
+++ filemanager/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from uuid import uuid4
 
 
 class FileManager(models.Model):
@@ -8,19 +9,20 @@ class FileManager(models.Model):
     File manager model to track uploaded files and their metadata.
     """
 
+    def upload_to(instance, filename):
+        """
+        Generate the upload path for the file.
+        """
+        return f"{instance.linked_model}/{instance.created_at.year}/{instance.created_at.month}/{instance.uuid}/{filename}"
+
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255)
-    object = models.FileField(upload_to="files/")
+    file = models.FileField(upload_to=upload_to)
     filesize = models.IntegerField()
     mimetype = models.CharField(max_length=100)
-    linked_model = models.CharField(max_length=100, null=True, blank=True)
-
-    def create(self, validated_data):
-        """
-        Save the file to the file manager and return the instance.
-        """
-        super().create(validated_data)
+    linked_model = models.CharField(max_length=100)
+    uuid = models.UUIDField(default=uuid4, editable=False)
 
 
 @receiver(post_delete, sender=FileManager)
