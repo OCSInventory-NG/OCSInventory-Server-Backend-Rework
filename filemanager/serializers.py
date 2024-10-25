15dec0fd4168635d396c7a5096fed2f0e24d4f89
Lea9250 <lea.droguet@factorfx.com>
2024-10-25 16:05:49 +0200

    refactor(FileManager): call compression from parent class

--- filemanager/serializers.py
+++ filemanager/serializers.py
@@ -1,34 +1,68 @@
 from rest_framework import serializers
 from .models import FileManager
-from rest_framework import serializers
-from .models import FileManager
 import mimetypes
+from uuid import uuid4
+
 
 class FileManagerSerializer(serializers.ModelSerializer):
     """
     Serializer for the FileManager model.
     """
-
     class Meta:
         model = FileManager
-        fields = "__all__"
-        read_only_fields = ["created_at", "modified_at", "filesize", "mimetype"]
+        fields = [
+            "id",
+            "created_at",
+            "modified_at",
+            "name",
+            "file",
+            "filesize",
+            "mimetype",
+            "linked_model",
+            "uuid",
+        ]
 
 
 class FileUploadMixin(serializers.Serializer):
     file = serializers.FileField(write_only=True, required=False)
 
+    def create(self, validated_data):
+        """
+        Create method to handle file upload
+        """
+        validated_data = self.handle_file_upload(validated_data)
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Update method to handle file upload
+        """
+        validated_data = self.handle_file_upload(validated_data)
+        return super().update(instance, validated_data)
+
     def handle_file_upload(self, validated_data):
+        """
+        Handles file compression and creates a FileManager object
+        """
         file = validated_data.pop('file', None)
         if file:
             mimetype = mimetypes.guess_type(file.name)[0]
+            compressed_file = self.compress(file)
+            uuid = uuid4()
             file_manager = FileManager.objects.create(
-                object=file,
-                name=file.name,
-                filesize=file.size,
+                file=compressed_file,
+                name=compressed_file.name,
+                filesize=compressed_file.size,
                 mimetype=mimetype,
-                # set linked model
-                linked_model=self.Meta.model.__name__
+                linked_model=self.Meta.model.__name__,
+                uuid=uuid
             )
             validated_data["file"] = file_manager
         return validated_data
+
+    def compress(self, file):
+        """
+        Default compression logic here, override in child classes as needed.
+        See DeploymentActionSerializer for an example of overriding this method.
+        """
+        return file
