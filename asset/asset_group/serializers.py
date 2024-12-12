ea6a8d33a1aeca62c51341432c97014b10cace5b
Charlene Auger <charlene.auger@factorfx.com>
2024-12-12 14:49:16 +0000

    refactor(assetgroup): override get serializer

--- asset/asset_group/serializers.py
+++ asset/asset_group/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-
+from django.core import serializers as ser
+from asset.inventory_base.models import InventoryBase
+from asset.inventory_base.serializers import InventoryBaseSerializer
 from .models import AssetGroup
 
 
@@ -10,10 +12,17 @@ class AssetGroupSerializer(serializers.ModelSerializer):
     Args:
         serializers ([ModelSerializer])
     """
-
     class Meta:
         """Define the linked model and the fields registered in the API"""
 
         model = AssetGroup
         fields = "__all__"
         extra_kwargs = {"last_updated": {"read_only": True}}
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['asset_bases'] = InventoryBaseSerializer(
+            InventoryBase.objects.filter(id__in=ret["assets"]), many=True
+        ).data
+
+        return ret
