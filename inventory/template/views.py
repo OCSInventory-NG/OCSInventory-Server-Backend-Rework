28dae011cf3dd3a2054ea7b43b9148e3ff4132eb
Antoine ROBIN <antoine.robin@factorfx.com>
2025-01-02 15:11:48 +0000

    feat(template): make legacy template undeletable

--- inventory/template/views.py
+++ inventory/template/views.py
@@ -2,6 +2,8 @@ from inventory.template.models import Template
 from inventory.template.serializers import TemplateSerializer
 from ocsinventory_backend.ocs_framework import viewsets
 from permission.permissions import DefaultModelPermissions
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class TemplateViewSet(viewsets.OCSViewSet):
@@ -18,3 +20,14 @@ class TemplateViewSet(viewsets.OCSViewSet):
     queryset = Template.objects.all()
     serializer_class = TemplateSerializer
     model = Template
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        legacy_tempalte = Template.objects.filter(name="Legacy", os="LEG")[0]
+        serializer = TemplateSerializer(legacy_tempalte)
+        if instance.id == serializer.data["id"]:
+            return Response(
+                {"detail": "Legacy template cannot be deleted"},
+                status.HTTP_401_UNAUTHORIZED,
+            )
+        return super().destroy(request)
