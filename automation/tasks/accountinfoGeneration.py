a31173f326aa728035ea642a71311e7f0307efa0
Lea9250 <lea.droguet@factorfx.com>
2025-01-31 16:26:15 +0100

    refactor(accountinfo): improve automation task

--- automation/tasks/accountinfoGeneration.py
+++ automation/tasks/accountinfoGeneration.py
@@ -1,9 +1,10 @@
 from automation.tasks.abstractTask import AbstractTask
 from asset.inventory_base.models import InventoryBase
 from accountinfo.views import AccountinfoDataViewSet
+from config.models import Config
 
 
-class AccountInfoCreation(AbstractTask):
+class AccountInfoGeneration(AbstractTask):
     """
     Task to create missing AccountinfoData entries for assets. This task
     is intended to be run using the automation/scheduler module.
@@ -13,8 +14,35 @@ class AccountInfoCreation(AbstractTask):
         """
         Find all assets without AccountinfoData and create entries for them
         """
-        # for all assets
-        assets = InventoryBase.objects.all()
+        if self.config_check():
+            assets = self.get_assets()
+            self.generate_accountinfo(assets)
 
-        for asset in assets:
-            AccountinfoDataViewSet.generate_accountinfo(asset, "inventory_base.inventorybase")
+    def config_check(self):
+        """
+        Check if accountinfo generation is set to automation mode
+        """
+        server_conf = Config.objects.filter(name="server").first()
+        for item in server_conf.value:
+            if item["name"] == "accountinfo_generation":
+                return item["value"] == "automation"
+        return False
+
+    def get_assets(self):
+        """
+        Get all assets that need accountinfo generation
+        """
+        return InventoryBase.objects.all()
+
+    def generate_accountinfo(self, assets):
+        """
+        Generate accountinfo for the given assets
+        """
+        try:
+            for asset in assets:
+                AccountinfoDataViewSet.generate_accountinfo(
+                    asset,
+                    "inventory_base.inventorybase"
+                )
+        except Exception as e:
+            print(f"Error generating accountinfo: {e}")
