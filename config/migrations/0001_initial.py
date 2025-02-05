a78c01ec2a2e9bc45a5c13f49ad2b2a9ae397a32
Lea9250 <lea.droguet@factorfx.com>
2025-02-05 17:48:37 +0100

    feat(config): replace debug switch with log level selection

--- config/migrations/0001_initial.py
+++ config/migrations/0001_initial.py
@@ -28,11 +28,12 @@ def create_base_config(apps, schema_editor):
             "name": "server",
             "value": [
                 {
-                    "name": "debug",
-                    "description": "Enable debug mode",
-                    "value": False,
-                    "type": "switch",
+                    "name": "log_level",
+                    "description": "Application log level",
+                    "value": "INFO",
+                    "type": "select",
                     "unit": "",
+                    "options": ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
                 },
                 {
                     "name": "duplicate_reconciliation",
@@ -123,7 +124,7 @@ def create_base_config(apps, schema_editor):
                 [
                     {
                         "name": "serialnumbers",
-                        "description": "Enabble serialnumbers blacklist",
+                        "description": "Enable serialnumbers blacklist",
                         "value": False,
                         "type": "switch",
                         "unit": "",
