a466dcc34864928eaaf9e61d2375387bb2ec8a40
gillesdubois <gillesdubois@users.noreply.github.com>
2025-03-19 16:05:47 +0000

    [MegaLinter] Apply linters fixes

--- tools/configure-ocsinventory-backend.sh
+++ tools/configure-ocsinventory-backend.sh
@@ -9,15 +9,15 @@ echo "================================================="
 echo ""
 
 if [[ ! -w "/usr/share/ocsinventory-backend/.env" ]]; then
-    echo "Your user doesn't have sufficient rights to allow the configuration of OCS Inventory Backend."
-    exit 1
+	echo "Your user doesn't have sufficient rights to allow the configuration of OCS Inventory Backend."
+	exit 1
 fi
 
 # remove default Nginx configuration
 if [ -f /etc/nginx/sites-enabled/default ]; then
-    echo "Removing default Nginx configuration..."
-    rm /etc/nginx/sites-enabled/default
-    echo "Default Nginx configuration removed."
+	echo "Removing default Nginx configuration..."
+	rm /etc/nginx/sites-enabled/default
+	echo "Default Nginx configuration removed."
 fi
 
 echo "Select the database engine:"
@@ -28,26 +28,26 @@ echo ""
 read -r -p "Database engine [1|2]: " db_engine
 
 case $db_engine in
-    1)
-        sed -i "s/DB_ENGINE=.*/DB_ENGINE='django.db.backends.postgresql'/" /usr/share/ocsinventory-backend/.env
-        echo "Database engine configured for PostgreSQL"
-        echo "Try to install PostgreSQL python library"
-        source /usr/lib/ocsinventory-backend/venv/bin/activate
-        pip3 install -r /usr/share/ocsinventory-backend/requirements_psql.txt
-        deactivate
-        ;;
-    2)
-        sed -i "s/DB_ENGINE=.*/DB_ENGINE='django.db.backends.mysql'/" /usr/share/ocsinventory-backend/.env
-        echo "Database engine configured for MySQL or MariaDB"
-        echo "Try to install MySQL/Mariadb python library"
-        source /usr/lib/ocsinventory-backend/venv/bin/activate
-        pip3 install -r /usr/share/ocsinventory-backend/requirements_mysql.txt
-        deactivate
-        ;;
-    *)
-        echo "Invalid option, aborted configuration !"
-        exit 1
-        ;;
+1)
+	sed -i "s/DB_ENGINE=.*/DB_ENGINE='django.db.backends.postgresql'/" /usr/share/ocsinventory-backend/.env
+	echo "Database engine configured for PostgreSQL"
+	echo "Try to install PostgreSQL python library"
+	source /usr/lib/ocsinventory-backend/venv/bin/activate
+	pip3 install -r /usr/share/ocsinventory-backend/requirements_psql.txt
+	deactivate
+	;;
+2)
+	sed -i "s/DB_ENGINE=.*/DB_ENGINE='django.db.backends.mysql'/" /usr/share/ocsinventory-backend/.env
+	echo "Database engine configured for MySQL or MariaDB"
+	echo "Try to install MySQL/Mariadb python library"
+	source /usr/lib/ocsinventory-backend/venv/bin/activate
+	pip3 install -r /usr/share/ocsinventory-backend/requirements_mysql.txt
+	deactivate
+	;;
+*)
+	echo "Invalid option, aborted configuration !"
+	exit 1
+	;;
 esac
 
 read -r -p "Which host is running database server ?: " db_host
@@ -70,31 +70,31 @@ echo "Now, running database migrations..."
 
 source /usr/lib/ocsinventory-backend/venv/bin/activate
 
-if python3 /usr/share/ocsinventory-backend/manage.py migrate > /tmp/ocsinventory-backend-configuration.log 2>&1; then
-    echo "Database migrations successfully applied !"
-    deactivate
+if python3 /usr/share/ocsinventory-backend/manage.py migrate >/tmp/ocsinventory-backend-configuration.log 2>&1; then
+	echo "Database migrations successfully applied !"
+	deactivate
 else
-    echo "Error during database migrations, please check /tmp/ocsinventory-backend-configuration.log for more information."
-    deactivate
-    exit 1
+	echo "Error during database migrations, please check /tmp/ocsinventory-backend-configuration.log for more information."
+	deactivate
+	exit 1
 fi
 
 # restart uWSGI service
 echo "Restarting uWSGI and Nginx services..."
 systemctl restart uwsgi
 if [ $? -eq 0 ]; then
-    echo "uWSGI service restarted successfully."
+	echo "uWSGI service restarted successfully."
 else
-    echo "Error restarting uWSGI service. Please check the service status manually."
-    exit 1
+	echo "Error restarting uWSGI service. Please check the service status manually."
+	exit 1
 fi
 
 systemctl restart nginx
 if [ $? -eq 0 ]; then
-    echo "Nginx service restarted successfully."
+	echo "Nginx service restarted successfully."
 else
-    echo "Error restarting Nginx service. Please check the service status manually."
-    exit 1
+	echo "Error restarting Nginx service. Please check the service status manually."
+	exit 1
 fi
 
 echo ""
