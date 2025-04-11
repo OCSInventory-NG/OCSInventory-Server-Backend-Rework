0fa32673eac6e5d7d125391dfbb27c497ebb54cc
Charlene Auger <charlene.auger@factorfx.com>
2025-04-11 12:29:53 +0000

    Revert "Merge branch 'section-category' of github.com:OCSInventory-NG/OCSInventory-Server-Backend-Rework into section-category"
    This reverts commit b8d5c8705388e759fc0f32774e966d45bfd91eef, reversing
    changes made to 00c6780d582ed34b9281dc03c083e32196e5551e.

--- reset_migration.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-git checkout auth config user inventory automation dashboard group #inventory/category
