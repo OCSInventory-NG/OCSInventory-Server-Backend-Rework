c6f14f0eaff920fe0bfdd376813872031167e3ce
hugotheault <hugo.theault.1@gmail.com>
2025-04-28 15:19:27 +0000

    allow blank in description field

--- search/models.py
+++ search/models.py
@@ -19,4 +19,4 @@ class Search(RestrictVisibility):
     search = JSONField()
     last_updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=50)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
