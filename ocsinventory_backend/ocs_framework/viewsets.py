02d2a56d849f50fd8e3f4f0f5d2ec54ef788deaa
Lea9250 <lea.droguet@factorfx.com>
2025-07-08 17:23:35 +0200

    refactor(viewset): add ordering filter and default ordering to OCSViewSet

--- ocsinventory_backend/ocs_framework/viewsets.py
+++ ocsinventory_backend/ocs_framework/viewsets.py
@@ -2,7 +2,7 @@ import logging
 
 from django.db.models import Q
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.response import Response
 
 
@@ -15,13 +15,17 @@ class OCSViewSet(viewsets.ModelViewSet):
     """
 
     # Set default filter
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
 
     # Filter on all by default
     filterset_fields = "__all__"
 
     # This is the default reconciliation id for objets, can overrided
     reconciliation_field = "id"
+
+    ordering_fields = '__all__'
+    # default ordering
+    ordering = ['id']
 
     logger = logging.getLogger("OCSViewSet")
 
