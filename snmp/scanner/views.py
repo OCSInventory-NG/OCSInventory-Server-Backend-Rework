451b9d76c574b2bd16334e2efe2056c9f3258223
Gilles DUBOIS <gilles.dubois@factorfx.com>
2025-08-12 18:12:39 +0200

    refactor(snmp/scanner): adapt ordering to views

--- snmp/scanner/views.py
+++ snmp/scanner/views.py
@@ -2,7 +2,7 @@ from ocsinventory_backend.ocs_framework import viewsets
 from permission.permissions import DefaultModelPermissions
 from snmp.scanner.models import SnmpScanner
 from snmp.scanner.serializers import SnmpScannerSerializer
-
+from rest_framework import filters
 
 class SnmpScannerViewSet(viewsets.OCSViewSet):
     """
@@ -13,7 +13,9 @@ class SnmpScannerViewSet(viewsets.OCSViewSet):
     """
 
     # Set default filter
-    filter_backends = []
+    filter_backends = [filters.OrderingFilter]
+    
+    ordering = ["identifier"]
 
     # Need to be authenticated to consult
     permission_classes = [DefaultModelPermissions]
