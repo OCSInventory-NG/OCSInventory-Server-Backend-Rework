0df9ca744f671755e23cbdc904aa499b91091c98
Charlene Auger <charlene.auger@factorfx.com>
2025-10-02 14:25:41 +0000

    refactor(viewsets): update delete return code

--- ocsinventory_backend/ocs_framework/viewsets.py
+++ ocsinventory_backend/ocs_framework/viewsets.py
@@ -58,7 +58,7 @@ class OCSViewSet(viewsets.ModelViewSet):
                 for id in request.data["ids"]:
                     instance = self.model.objects.get(id=id)
                     self.perform_destroy(instance)
-                return Response(status=status.HTTP_200_OK)
+                return Response(status=status.HTTP_204_NO_CONTENT)
             else:
                 serializer = self.get_serializer(
                     data=request.data, many=isinstance(request.data, list)
