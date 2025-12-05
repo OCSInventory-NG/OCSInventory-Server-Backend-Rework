4671aa5c998b40f24f216f30ab5b40b1299e7982
Charlene Auger <charlene.auger@factorfx.com>
2025-12-05 10:21:42 +0000

    refactor(netdevice): add filtering

--- ipdiscover/netdevice/views.py
+++ ipdiscover/netdevice/views.py
@@ -2,6 +2,7 @@ from ipdiscover.netdevice.models import Netdevice
 from ipdiscover.netdevice.serializers import NetdeviceSerializer
 from permission.permissions import DefaultModelPermissions
 from ocsinventory_backend.ocs_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class NetdeviceViewSet(viewsets.OCSViewSet):
@@ -18,3 +19,17 @@ class NetdeviceViewSet(viewsets.OCSViewSet):
     queryset = Netdevice.objects.all()
     serializer_class = NetdeviceSerializer
     model = Netdevice
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = [
+        'ip',
+        'netname',
+        'mac',
+        'last_seen'
+    ]
+    ordering_fields = [
+        'ip',
+        'netname',
+        'mac',
+        'network',
+        'last_seen'
+    ]
