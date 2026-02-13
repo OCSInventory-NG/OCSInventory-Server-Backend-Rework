e68f876923a5c9ebad2e6a150716934f03e68c79
Lea9250 <lea.droguet@factorfx.com>
2026-02-13 09:08:15 +0000

    fix(extension): keep post_migrate sync receiver persistent

--- extension/apps.py
+++ extension/apps.py
@@ -2,6 +2,12 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
 
+def _sync_extensions_post_migrate(**kwargs):
+    from .sync import sync_extensions_from_filesystem
+
+    sync_extensions_from_filesystem()
+
+
 class ExtensionConfig(AppConfig):
     """
     Base definition of the django app
@@ -14,10 +20,9 @@ class ExtensionConfig(AppConfig):
     name = "extension"
 
     def ready(self):
-        from .sync import sync_extensions_from_filesystem
-
         post_migrate.connect(
-            lambda **kwargs: sync_extensions_from_filesystem(),
+            _sync_extensions_post_migrate,
             sender=self,
             dispatch_uid="extension_post_migrate_sync",
+            weak=False,
         )
