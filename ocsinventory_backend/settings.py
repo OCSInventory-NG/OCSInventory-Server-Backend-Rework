ae407d468a6de46687443838dd1715e2ee97beb1
faupointalexandre <alexandre.faupoint@factorfx.com>
2026-03-05 17:56:54 +0100

    Remove auth_views logging configuration
    Removed debug logging configuration for auth views.

--- ocsinventory_backend/settings.py
+++ ocsinventory_backend/settings.py
@@ -174,11 +174,6 @@ LOGGING = {
             "level": "INFO",
             "propagate": False,
         },
-        "auth_views": {
-            "handlers": ["ocs_auth"],
-            "level": "DEBUG",
-            "propagate": False,
-        },
         # inventory collection  only
         "asset.collection.views": {
             "handlers": ["ocs_collection"],
