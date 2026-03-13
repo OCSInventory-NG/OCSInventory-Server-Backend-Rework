e8a1e830e66c033764d5398d3c3bb13817d93542
noepoirier <noepoirier@users.noreply.github.com>
2026-03-13 16:07:28 +0000

    [MegaLinter] Apply linters fixes

--- search/views.py
+++ search/views.py
@@ -74,7 +74,9 @@ class SearchView(GenericAPIView):
                 if masterindex > 0 and index == 0:
                     links[masterindex] = condition["link"]
 
-                if (operator in TEXT_OPERATORS and isinstance(value, int)) or value == "":
+                if (
+                    operator in TEXT_OPERATORS and isinstance(value, int)
+                ) or value == "":
                     operator = "exact"
 
                 # Construction of the Q condition
