54a2f7076d6cc99a8ad911ff3457cf9b1edbfdee
Lea9250 <lea.droguet@factorfx.com>
2026-09-24 14:53:40 +0000

    fix(migration): fix wrong mig dependency

--- virtualcolumn/migrations/0001_initial.py
+++ virtualcolumn/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("auth", "0014_remove_group_is_protected"),
+        ("auth", "0012_alter_user_first_name_max_length"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
